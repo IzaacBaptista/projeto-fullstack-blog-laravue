64939d5ebed69f1c655888e2124ef615387b1802
Restyled.io <commits@restyled.io>
2024-03-27 11:10:45 +0000

    Restyled by clang-format

--- blog-app/src/http/blog-api.js
+++ blog-app/src/http/blog-api.js
@@ -6,6 +6,7 @@ export const allArticles = () => api.get(resource);
 
 export const createArticle = (article) => api.post(resource, article);
 
-export const updateArticle = (id, article) => api.put(`${resource}/${id}`, article);
+export const updateArticle = (id, article) =>
+    api.put(`${resource}/${id}`, article);
 
 export const removeArticle = (id) => api.delete(`${resource}/${id}`);
