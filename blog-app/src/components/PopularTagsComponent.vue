59c0dea579b26cfa6f4588af8be6a13c1e9ce861
Izaac Baptista <izaacbaptista@gmail.com>
2024-04-03 09:09:32 -0300

    feat(categories) add session popular tags

--- blog-app/src/components/PopularTagsComponent.vue
+++ blog-app/src/components/PopularTagsComponent.vue
@@ -1,14 +1,35 @@
 <template>
     <section class="popular-tags section">
-
-        <div class="container">
-
-            <h2 class="title section-title" data-name="Popular tags">Popular tags</h2>
-
-            <div class="popular-tags-container d-grid">
-
-            </div>
-
+      <div class="container">
+        <h2 class="title section-title" data-name="Popular tags">Popular tags</h2>
+        <div class="popular-tags-container d-grid">
+          <PopularTags v-for="(category, index) in popularTags" :key="category.id" :index="index" :category="category" />
         </div>
+      </div>
     </section>
-</template>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref } from "vue";
+  import { allCategories } from "../http/blog-api";
+  import PopularTags from "./articles/PopularTags.vue";
+  
+  const popularTags = ref([]);
+  
+  onMounted(async () => {
+    const response = await allCategories();
+    const categories = response.data.data;
+
+    const shuffledCategories = shuffle(categories);
+    popularTags.value = shuffledCategories.slice(0, 6);
+  });
+
+  const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+  </script>
+  
---
+++ blog-app/src/components/articles/PopularTags.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="'./categorias.html?category=' + category.name.toLowerCase()" class="article">
+    <span class="tag-name">#{{ category.name }}</span>
+    <img :src="'/images/categories/' + category.name.toLowerCase() + '.jpg'" alt="" class="article-image">
+  </a>
+</template>
+
+
+<script setup>
+    defineProps({
+        category: Object,
+        index: Number
+    });
+</script>
